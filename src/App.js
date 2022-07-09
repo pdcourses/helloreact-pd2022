b61bc0cb7164bd80790e1724b6e7714c8118f1b0
pdcourses <freshitpt@gmail.com>
2022-07-09 13:38:11 +0300

    example fun component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './Components/Greeting';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// функциональный компонент App
+// return! только 1 элемент!!!!!
+function App(){
+  return(
+    <Greeting />
   );
 }
-
 export default App;
---
+++ src/Components/Greeting/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Greeting(){
+    return(
+        <>
+            <p>Greeting Component</p>
+            <p>Hello, User</p>
+        </>
+    );
+}
+
+export default Greeting;
